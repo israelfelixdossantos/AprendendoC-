58225cb6a90742ea107eac0a1265c2c683347ca3
Israel felix dos Santos <grrifs@gmail.com>
2024-11-26 08:25:28 -0300

    Primeiros passos em C#

---
+++ 02variaveis/Program.cs
@@ -0,0 +1,16 @@
+﻿// Textos
+string nome = "Ralf";
+// Letras
+char inicial = 'r';
+// Números inteiros
+int idade = 34;
+// Números reais
+double altura = 1.79;
+// Lógica
+bool estuda = true;
+
+Console.WriteLine(nome);
+Console.WriteLine(inicial);
+Console.WriteLine(idade);
+Console.WriteLine(altura);
+Console.WriteLine(estuda);
---
+++ 03constantes/Program.cs
@@ -0,0 +1,13 @@
+﻿// Criar uma variavel
+
+const string nome = "Israel";
+
+// Alterar valor da variável 
+
+// nome = "Lucas";
+
+// Exibir o valor da variável
+
+Console.WrireLine(nome);
+
+
