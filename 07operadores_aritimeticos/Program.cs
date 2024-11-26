e9574a1605390d192e7d26356762d284bd9c382a
Israel felix dos Santos <grrifs@gmail.com>
2024-11-26 09:19:18 -0300

    Operadores Aritimeticos

---
+++ 07operadores_aritimeticos/Program.cs
@@ -0,0 +1,28 @@
+﻿// Variáveis
+
+int numero1 = 30;
+int numero2 = 3;
+
+
+// Divisão
+Console.WriteLine(numero1 / numero2);
+
+// Multiplicação
+Console.WriteLine(numero1 * numero2);
+
+// Subtração
+Console.WriteLine(numero1 - numero2);
+
+// Soma
+Console.WriteLine(numero1 + numero2);
+
+// Modulo (resto da divisão)
+Console.WriteLine(numero1 % numero2);
+
+// Incrementador
+numero1++;
+Console.WriteLine(numero1);
+
+// Decrementador
+numero1--;
+Console.WriteLine(numero1);
