3838fe54990b31b42f6336e6e20c8f0288acafa3
Israel felix dos Santos <grrifs@gmail.com>
2024-11-26 09:07:13 -0300

    Conversão

---
+++ 06conversao/Program.cs
@@ -0,0 +1,11 @@
+﻿// Obter o primeiro numero
+Console.WriteLine("Informe um número");
+int numero1 = Convert.ToInt16(Console.ReadLine());
+
+
+// Obter o segundo numero
+Console.WriteLine("Informe outro número");
+int numero2 = Convert.ToInt16(Console.ReadLine());
+
+//Realizar a soma
+Console.WriteLine($"A some é: {numero1 + numero2}");
