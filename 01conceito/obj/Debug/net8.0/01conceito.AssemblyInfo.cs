825fe270b738c6144cebdff3488cab0b2e86bb1d
Israel felix dos Santos <grrifs@gmail.com>
2024-11-27 08:34:20 -0300

    Condicional Simples - If e else

--- 01conceito/obj/Debug/net8.0/01conceito.AssemblyInfo.cs
+++ 01conceito/obj/Debug/net8.0/01conceito.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01conceito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4218e110f4c13814d481580e9d3ac50f8c53d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("01conceito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01conceito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Comandos.md
@@ -0,0 +1,4 @@
+1 Criar o projeto - dotnet new console -n "nome do projeto" 
+2 Executar projeto - dotnet run
+3 
+
