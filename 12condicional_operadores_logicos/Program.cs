d5341afe4f6c6cf636a1b0992739f5dfe7a574fe
Israel felix dos Santos <grrifs@gmail.com>
2024-11-27 08:51:18 -0300

    Condicional Operadores Logicos

---
+++ 12condicional_operadores_logicos/Program.cs
@@ -0,0 +1,11 @@
+﻿double valor 500;
+string pagamento = "à vista";
+
+if(valor >= 100 && pagamento == "à vista")
+{
+    Console.WriteLine(&"Desconto de 10%! Total a pagar é R${valor*0.9}");
+}
+else
+{
+    Console.WriteLine(&"O total a pagar é R${valor}");
+}
