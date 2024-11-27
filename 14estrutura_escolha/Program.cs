f7c60976aa85d7300ed1ac77d8d71f0ea15641e8
Israel felix dos Santos <grrifs@gmail.com>
2024-11-27 09:48:24 -0300

    Estrutura de escolha

---
+++ 14estrutura_escolha/Program.cs
@@ -0,0 +1,23 @@
+﻿string linguagem = "HTML";
+
+
+// Estrutura de escolha
+
+switch (linguagem)
+{
+    case "HTML":
+    Console.WriteLine("Liguagem de Marcação.");
+    break;
+
+    case "CSS":
+    Console.WriteLine("Liguagem de estilos.");
+    break;
+
+    case "C#":
+    Console.WriteLine("Liguagem de programação.");
+    break;
+
+    default:
+    Console.WriteLine("Liguagem de desconhecida.");
+    break;
+}
