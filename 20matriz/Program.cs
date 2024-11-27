a19a8dce6be14b45ae74a36cf799b475a6388cf3
Israel felix dos Santos <grrifs@gmail.com>
2024-11-27 11:29:05 -0300

    Matriz

---
+++ 20matriz/Program.cs
@@ -0,0 +1,15 @@
+﻿// Matriz
+string[,] dados = {
+    {"Ralf", "Curitiba"},
+    {"Juliana", "Campinas"},
+    {"Cleber", "Joinvile"},
+    {"Marcela", "Londrina"}
+};
+
+// Exibir dados
+// Console.WriteLine(dados[1, 0]);
+
+foreach (string dado in dados)
+{
+    Console.WriteLine(dado);
+}
