45325aac36b02f8bb8a33186867482edd05d8180
Israel felix dos Santos <grrifs@gmail.com>
2024-11-27 11:06:28 -0300

    Vetor

---
+++ 18vetor/Program.cs
@@ -0,0 +1,16 @@
+﻿// Verto de nomes
+string[] nomes = {"Ralf", "juliana", "Cleber", "Marcela"};
+
+// Exibir os nomes
+
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);
+
+
+for (int i = 0; i <= nomes.Length; i++)
+{
+   Console.WriteLine(nomes[i] + " - for"); 
+};
+
