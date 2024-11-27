ab15fb9b0b1744313ffee1ebe8eb14ab4a0d7c06
Israel felix dos Santos <grrifs@gmail.com>
2024-11-27 08:43:44 -0300

    Condicional encadeada else if

---
+++ 11condicional_encadeada/Program.cs
@@ -0,0 +1,14 @@
+﻿double media = 8
+
+if(media >= 7)
+{
+    Console.WriteLine("Aprovado(a)");
+}
+else if (media >= 5)
+{
+    Console.WriteLine("Em Exame(a)");
+}
+else
+{
+    Console.WriteLine("Reprovado(a)");
+}
